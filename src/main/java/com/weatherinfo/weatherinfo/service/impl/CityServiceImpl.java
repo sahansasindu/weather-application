f464b7915f659c62f065ffea1e404aab26f1f673
sahansasindu <sahan_2020742@fot.ruh.ac.lk>
2025-08-09 12:07:42 +0530

    add

--- src/main/java/com/weatherinfo/weatherinfo/service/impl/CityServiceImpl.java
+++ src/main/java/com/weatherinfo/weatherinfo/service/impl/CityServiceImpl.java
@@ -1,4 +1,30 @@
 package com.weatherinfo.weatherinfo.service.impl;
 
-public class CityServiceImpl {
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.weatherinfo.weatherinfo.service.CityService;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CityServiceImpl implements CityService {
+    @Override
+    public List<String> getCityCodes() {
+        List<String> cityCodes = new ArrayList<>();
+        try {
+
+            ClassPathResource resource = new ClassPathResource("cities.json");
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode rootNode = mapper.readTree(resource.getInputStream());
+
+            for (JsonNode city : rootNode.get("List")) {
+                cityCodes.add(city.get("CityCode").asText());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading cities", e);
+        }
+        return cityCodes;
+    }
 }
---
+++ src/main/java/com/weatherinfo/weatherinfo/service/impl/WeatherServiceImpl.java
@@ -0,0 +1,13 @@
+package com.weatherinfo.weatherinfo.service.impl;
+
+import com.weatherinfo.weatherinfo.dto.WeatherInfoResponseDto;
+import com.weatherinfo.weatherinfo.service.WeatherService;
+
+import java.util.List;
+
+public class WeatherServiceImpl implements WeatherService {
+    @Override
+    public List<WeatherInfoResponseDto> getWeatherForAllCities() {
+        return null;
+    }
+}
