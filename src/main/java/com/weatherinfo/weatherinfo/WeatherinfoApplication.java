e584059a063e12e8da61952f8bcfd56615ad96f7
sahansasindu <sahan_2020742@fot.ruh.ac.lk>
2025-08-09 10:40:32 +0530

    first commit

---
+++ src/main/java/com/weatherinfo/weatherinfo/WeatherinfoApplication.java
@@ -0,0 +1,13 @@
+package com.weatherinfo.weatherinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherinfoApplication.class, args);
+	}
+
+}
