1c4f454eec3dde421fa3a482ddb61f8662303ad5
sahansasindu <sahan_2020742@fot.ruh.ac.lk>
2025-08-09 11:34:26 +0530

    Update WeatherInfoResponseDto.java

--- src/main/java/com/weatherinfo/weatherinfo/dto/WeatherInfoResponseDto.java
+++ src/main/java/com/weatherinfo/weatherinfo/dto/WeatherInfoResponseDto.java
@@ -1,4 +1,18 @@
 package com.weatherinfo.weatherinfo.dto;
 
 public class WeatherInfoResponseDto {
+    private String cityName;
+    private String description;
+    private double temperature;
+
+    public WeatherInfoResponseDto(String cityName, String description, double temperature) {
+        this.cityName = cityName;
+        this.description = description;
+        this.temperature = temperature;
+    }
+
+    public String getCityName() { return cityName; }
+    public String getDescription() { return description; }
+    public double getTemperature() { return temperature; }
 }
+
