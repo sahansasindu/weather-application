af00360ed1197edfd80d579174a06a54c7ce1831
sahansasindu <sahan_2020742@fot.ruh.ac.lk>
2025-08-09 11:33:15 +0530

    Update LoginController.java

--- src/main/java/com/weatherinfo/weatherinfo/controller/LoginController.java
+++ src/main/java/com/weatherinfo/weatherinfo/controller/LoginController.java
@@ -1,4 +1,17 @@
 package com.weatherinfo.weatherinfo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoginController {
+
+
+
+    @GetMapping("/")
+    public String homeRedirect() {
+
+        return "redirect:/oauth2/authorization/auth0";
+    }
+
 }
